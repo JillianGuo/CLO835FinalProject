826ed1b794e5efde3bf9d18c1c812460e5bd7678
Jillian Guo <jguo73@myseneca.ca>
2025-04-11 15:23:37 -0400

    Add aws credential manifest

--- app/get_pic.py
+++ app/get_pic.py
@@ -5,7 +5,12 @@ def download_pic(pic_name, bucket_name, des_path):
     """
     Function to download a pic from an S3 bucket
     """
-    s3 = boto3.client('s3')
+    s3 = boto3.client(
+        's3', 
+        aws_access_key_id=os.getenv('aws_access_key_id'),
+        aws_secret_access_key=os.getenv('aws_secret_access_key'), 
+        aws_session_token=os.getenv('aws_session_token')
+    )
     
     # Create destination directory if it doesn't exist
     os.makedirs(des_path, exist_ok=True)
